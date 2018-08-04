e785cf291783931af5f95eec83b586a7e12b719e
Leonardo <Leonardo@DESKTOP-1FAQTT8>
2018-08-04 20:33:49 -0300

    auth;

--- views/inc/auth.php
+++ views/inc/auth.php
@@ -8,6 +8,7 @@ $params = anti_injection($_POST);
 if (empty($_COOKIE['tos_key'])) {
     if ($params['senha'] == 'topzera.') {
         setcookie("tos_key", base64_encode(serialize(md5('topzera.'))), time() + 60 * 60 * 24 * 7, "/", "", "", true);
+        header('location: /principal');
     } else {
         $_SESSION['error'] = 'Não tem nada aqui, vai embora.. ';
         header('location:' . URLANTERIOR);
