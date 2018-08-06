5ece9dc5f12e18937363d29e1782ffccbeac8fd7
Leonardo <Leonardo@DESKTOP-1FAQTT8>
2018-08-05 21:57:28 -0300

    skill e gema da galera;

--- views/principal.php
+++ views/principal.php
@@ -16,6 +16,37 @@ include_once HEADER;
             </li>
         </ul>
     </div>
+
+    <div class="col-md-12">
+        <h2>Gema - Skill PET</h2>
+        - LordHeizem
+        <ul>
+            <li><img src="/arquivos/images/Confusão.png" width="35px" height="35px"> CHS</li>
+            <li><img src="/arquivos/images/FOGO.png" width="35px" height="35px"> BRN</li>
+        </ul>
+        Skill pet: elfo
+        <hr>
+        - Radamanthys
+        <ul>
+            <li><img src="/arquivos/images/Stun.png" width="35px" height="35px"> STN</li>
+            <li><img src="/arquivos/images/veneno.png" width="35px" height="35px"> POI</li>
+        </ul>
+        Skill pet: dragao
+        <hr>
+        - LadyFeity
+        <ul>
+            <li><img src="/arquivos/images/Silencio.png" width="35px" height="35px"> SIL</li>
+            <li><img src="/arquivos/images/sangrar.png" width="35px" height="35px"> BLD</li>
+        </ul>
+        Skill pet: humanoide
+        <hr>
+        - Kiera
+        <ul>
+            <li><img src="/arquivos/images/Petrificar.png" width="35px" height="35px"> PTF</li>
+            <li><img src="/arquivos/images/Sono.png" width="35px" height="35px"> SLB</li>
+        </ul>
+        Skill pet: elfo ? planta
+    </div>
 </div>
 
 <?php
